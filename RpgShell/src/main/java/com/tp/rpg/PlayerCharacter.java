a62b0c1169f415832315db6e16a0899e841a601e
Noel Castillo <noelcastillo@C02DQ7WHML7H.local>
2021-01-22 18:01:23 -0500

    Adding Rpg to repo

---
+++ RpgShell/src/main/java/com/tp/rpg/PlayerCharacter.java
@@ -0,0 +1,9 @@
+package com.tp.rpg;
+
+public class PlayerCharacter extends Character {
+    //use scanner here to get something from the user
+    @Override
+    public String makeChoice() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ RpgShell/src/main/java/com/tp/rpg/armors/Shirt.java
@@ -0,0 +1,8 @@
+package com.tp.rpg.armors;
+
+public class Shirt extends Armor  {
+    @Override
+    public int reduceDamage(int startingDamage) {
+        throw new UnsupportedOperationException();
+    }
+}
