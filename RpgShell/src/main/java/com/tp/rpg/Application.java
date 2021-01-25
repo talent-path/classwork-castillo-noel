fc8ebc22d1b8191458651655b11ffeac4247b219
Noel Castillo <noelcastillo@C02DQ7WHML7H.local>
2021-01-25 13:06:57 -0500

    enemy set up and overall plot established

--- RpgShell/src/main/java/com/tp/rpg/Application.java
+++ RpgShell/src/main/java/com/tp/rpg/Application.java
@@ -4,6 +4,10 @@ import com.tp.rpg.armors.Chainmail;
 import com.tp.rpg.armors.Leather;
 import com.tp.rpg.armors.Plate;
 import com.tp.rpg.armors.Robe;
+import com.tp.rpg.enemies.Dragon;
+import com.tp.rpg.enemies.Goblin;
+import com.tp.rpg.enemies.Orc;
+import com.tp.rpg.enemies.Troll;
 import com.tp.rpg.items.*;
 import com.tp.rpg.weapons.*;
 
@@ -15,16 +19,30 @@ import java.util.*;
 public class Application {
 
     // V A R I A B L E S
-    public int enemiesDefeated = 0;
+    public static int dragonsDefeated = 0;
+    public static int trollsDefeated = 0;
+    public static int orcsDefeated = 0;
+    public static int goblinsDefeated = 0;
 
     // M A I N
     public static void main(String[] args) {
         PlayerCharacter pc = setUpPlayer();
 
-        while (pc.isAlive()) {
+        while (pc.isAlive() && dragonsDefeated == 0) {
             NonPlayerCharacter enemy = setUpEnemy();
 
             battle(pc, enemy);
+            if(pc.isAlive()){
+                if(enemy.getClass().equals("class Dragon")) {
+                    dragonsDefeated++;
+                } else if(enemy.getClass().equals("class Troll")){
+                    trollsDefeated++;
+                } else if(enemy.getClass().equals("class Orc")){
+                    orcsDefeated++;
+                } else{
+                    goblinsDefeated++;
+                }
+            }
 
         }
 
@@ -81,45 +99,83 @@ public class Application {
 
     //create some NPC object (with armor & weapons?)
     private static NonPlayerCharacter setUpEnemy() {
-
-
-        throw new UnsupportedOperationException();
+        Random rng = new Random();
+        int enemyRng = rng.nextInt(11);
+        if(enemyRng < 5){
+            return new Goblin();
+        } else if(enemyRng >= 5 && enemyRng <= 7){
+            return new Orc();
+        } else if(enemyRng > 7 && enemyRng < 10){
+            return new Troll();
+        } else{
+            return new Dragon();
+        }
     }
 
     //a and b battle until one is dead
-    private static void battle(Character a, Character b) {
-        Character attacker = a;
-        Character defender = b;
+    private static PlayerCharacter battle(PlayerCharacter pc, NonPlayerCharacter enemy) {
 
-        while (a.isAlive() && b.isAlive()) {
-            if (a.makeChoice().equals("Attack")) {
-                attacker.attack(defender);
-            } else if(a.makeChoice().equals("Use Item")){
+        boolean pcTurn = true;
+        int battleSelection = Console.readInt("Select your weapon:" +
+                "\n(1) Axe\n(2) Bow\n(3) Sword\n(4) Dagger", 1, 4);
+
+        while (pc.isAlive() && enemy.isAlive()) {
+            if (pc.makeChoice().equals("Attack")) {
+                pc.attack(enemy);
+            } else if(pc.makeChoice().equals("Use Item")){
                 //TODO: consider other actions
                 throw new UnsupportedOperationException();
             } else {
-                System.out.println(a.getName() + " decides to run away!");
+                System.out.println(pc.getName() + " decides to run away!");
                 break;
             }
 
             //TODO: display HP status?
-            System.out.println(a.getName() + "'s current HP: " + a.getHp());
-            System.out.println(b.getName() + "'s current HP: " + b.getHp());
-            Character temp = a;
-            a = b;
-            b = temp;
+            System.out.println(pc.getName() + "'s current HP: " + pc.getHp());
+            System.out.println(enemy.getName() + "'s current HP: " + enemy.getHp());
         }
+
+        return pc;
     }
 
     //display some message
     private static void gameOverScreen() {
+        if(dragonsDefeated == 0){
+            System.out.println("Your life has been lost in the forest...");
+        } else {
+            System.out.println("You've slain the Dragon and left the forest as a Hero!");
+        }
     }
 
-    public int getEnemiesDefeated() {
-        return enemiesDefeated;
+    public static int getDragonsDefeated() {
+        return dragonsDefeated;
     }
 
-    public void setEnemiesDefeated(int enemiesDefeated) {
-        this.enemiesDefeated = enemiesDefeated;
+    public static void setDragonsDefeated(int dragonsDefeated) {
+        Application.dragonsDefeated = dragonsDefeated;
+    }
+
+    public static int getTrollsDefeated() {
+        return trollsDefeated;
+    }
+
+    public static void setTrollsDefeated(int trollsDefeated) {
+        Application.trollsDefeated = trollsDefeated;
+    }
+
+    public static int getOrcsDefeated() {
+        return orcsDefeated;
+    }
+
+    public static void setOrcsDefeated(int orcsDefeated) {
+        Application.orcsDefeated = orcsDefeated;
+    }
+
+    public static int getGoblinsDefeated() {
+        return goblinsDefeated;
+    }
+
+    public static void setGoblinsDefeated(int goblinsDefeated) {
+        Application.goblinsDefeated = goblinsDefeated;
     }
 }
