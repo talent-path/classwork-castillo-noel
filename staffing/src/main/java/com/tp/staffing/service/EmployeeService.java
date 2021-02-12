d31e43caed30cddb9cc42f6c25353c5fd7791b74
Noel Castillo <noelcastillo@C02DQ7WHML7H.local>
2021-02-12 17:52:19 -0500

    Added comments for service layer methods.

--- staffing/src/main/java/com/tp/staffing/service/EmployeeService.java
+++ staffing/src/main/java/com/tp/staffing/service/EmployeeService.java
@@ -15,6 +15,9 @@ public class EmployeeService {
     @Autowired
     EmployeePostgresDao dao;
 
+    //Adds an employee to the database.
+    //When adding an employee, an exception will be thrown if either first name or last name variables
+    //are null or empty strings.
     public Integer addEmployee(Employee employee) throws NullEmployeeFirstNameException, NullEmployeeLastNameException, InvalidEmployeeFirstNameException, InvalidEmployeeLastNameException {
 
         if (employee.getFirstName() == null) {
@@ -32,6 +35,8 @@ public class EmployeeService {
         return dao.addEmployee(employee);
     }
 
+    //Retrieves an employee from the database by it's id.
+    //When retrieving an employee from the database. An exception will be thrown if given a null for the id.
     public Employee getEmployeeById(Integer id) throws NullEmployeeIdException, InvalidEmployeeIdException {
         if (id == null) {
             throw new NullEmployeeIdException("You cannot retrieve an Employee with null id.");
@@ -45,6 +50,9 @@ public class EmployeeService {
         }
     }
 
+    //Retrieves a list of employees from the database by a given last name to check for.
+    //When retrieving a list of employees by last name. An exception will be thrown if
+    //the given last name to check for is either null or an empty string.
     public List<Employee> getEmployeesByLastName(String lastName) throws NullEmployeeLastNameException, InvalidEmployeeLastNameException {
         if (lastName == null) {
             throw new NullEmployeeLastNameException("You cannot retrieve Employees with null last name.");
@@ -56,10 +64,14 @@ public class EmployeeService {
         return dao.getEmployeesByLastName(lastName);
     }
 
+    //Retrieves a list of all employees in the database.
     public List<Employee> getEmployees() {
         return dao.getEmployees();
     }
 
+    //Updates an existing employee from the database.
+    //When editing an existing employee. An exception will be thrown if attempted to update first or last name
+    //variables with a null or empty string.
     public boolean editEmployee(Integer id, Employee employee) throws NullEmployeeFirstNameException, NullEmployeeLastNameException, InvalidEmployeeFirstNameException, InvalidEmployeeLastNameException {
         if (employee.getFirstName() == null) {
             throw new NullEmployeeFirstNameException("You cannot edit a Employee to have a null first name.");
@@ -76,6 +88,8 @@ public class EmployeeService {
         return dao.editEmployee(id, employee);
     }
 
+    //Deletes an existing employee from the database.
+    //When deleting an employee, an exception will be thrown if provided with a null for the id.
     public boolean deleteEmployee(Integer id) throws NullEmployeeIdException {
         if (id == null) {
             throw new NullEmployeeIdException("You cannot delete a Employee with null id.");
--- staffing/src/main/java/com/tp/staffing/service/PositionService.java
+++ staffing/src/main/java/com/tp/staffing/service/PositionService.java
@@ -16,6 +16,9 @@ public class PositionService {
     @Autowired
     PositionPostgresDao dao;
 
+    //Adds a position to the database.
+    //When attempting to add a position to the database. An exception will be thrown if the given title
+    //variable is either null or empty.
     public Integer addPosition(Position position) throws NullPositionTitleException, InvalidPositionTitleException {
         if (position.getTitle() == null) {
             throw new NullPositionTitleException("You cannot add a Position with a null title.");
@@ -27,6 +30,9 @@ public class PositionService {
         return dao.addPosition(position);
     }
 
+    //Retrieves a position from the database by id.
+    //When attempting to retrieve an id from the database. An exception will be thrown if given
+    //a null for the id to look for.
     public Position getPositionById(Integer id) throws NullPositionIdException, InvalidPositionIdException {
         if (id == null) {
             throw new NullPositionIdException("You cannot retrieve a Position with null id.");
@@ -41,6 +47,8 @@ public class PositionService {
 
     }
 
+    //Retrieves a list of positions by title. When getting a list of positions from the database by a given title, an exception will be
+    //thrown if the given title to check for is either null or empty.
     public List<Position> getPositionsByTitle(String title) throws NullPositionTitleException, InvalidPositionTitleException {
         if (title == null) {
             throw new NullPositionTitleException("You cannot retrieve Positions with a null title.");
@@ -52,10 +60,14 @@ public class PositionService {
         return dao.getPositionsByTitle(title);
     }
 
+    //Retrieves a list of all existing positions in the database.
     public List<Position> getPositions() {
         return dao.getPositions();
     }
 
+    //Updates an existing position in the database.
+    //An exception will be thrown if attempting to update a position to have a
+    //null or empty string for the title.
     public boolean editPosition(Integer id, Position position) throws InvalidPositionTitleException, NullPositionTitleException, InvalidPositionIdException {
         if (position.getTitle() == null) {
             throw new NullPositionTitleException("You cannot edit a Position to have a null title.");
@@ -67,6 +79,8 @@ public class PositionService {
         return dao.editPosition(id, position);
     }
 
+    //Deletes ane existing position from the database.
+    //An exception will be thrown if given a null for the id to check for when deleting.
     public boolean deletePosition(Integer id) throws NullPositionIdException, InvalidPositionIdException {
         if (id == null) {
             throw new NullPositionIdException("You cannot delete a Position with null id.");
@@ -74,6 +88,8 @@ public class PositionService {
         return dao.deletePosition(id);
     }
 
+    //Adds an employee id to a position to establish a relationship.
+    //An exception will be thrown if either the employee id or position id are null
     public boolean addEmployeeToPosition(Integer employeeId, Integer positionId) throws NullEmployeeIdException, NullPositionIdException, InvalidPositionIdException {
         if (employeeId == null) {
             throw new NullEmployeeIdException("You cannot retrieve an Employee with null id.");
@@ -86,6 +102,8 @@ public class PositionService {
     }
 
 
+    //Removes any existing employee id from a position by the given position id.
+    //An exception will be thrown if given a null for the position id.
     public boolean removeEmployeeFromPosition(Integer id) throws NullPositionIdException, InvalidPositionIdException {
         if (id == null) {
             throw new NullPositionIdException("Cannot locate a Position with null id.");
